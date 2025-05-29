e173837accaf65458200388f425c7c6ca08cd039
Gustavo Cervantes <gcervantesmm@gmail.com>
2025-05-29 20:58:10 +0000

    Error... 404 This page could not be found

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -17,6 +18,15 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/builder/:poaId/procedure-description',
+        destination: '/builder/:poaId/introduction',
+        permanent: true,
+      },
+    ]
   },
 };
 
